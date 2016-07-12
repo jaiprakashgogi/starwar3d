00330ee7e24e293c609d0ff0de98d99c25cb12d8
Jai Prakash <jaiprakashgogi@gmail.com>
2016-07-12 10:05:06 -0700

    Adding the pointer for shooting

--- gvr-starwar/app/src/main/java/org/gearvrf/starwar/PlanesManager.java
+++ gvr-starwar/app/src/main/java/org/gearvrf/starwar/PlanesManager.java
@@ -5,8 +5,12 @@ import android.media.Image;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.gearvrf.GVRAndroidResource;
 import org.gearvrf.GVRContext;
+import org.gearvrf.GVRSceneObject;
 import org.gearvrf.GVRScript;
+import org.gearvrf.GVRTexture;
+import org.gearvrf.GVRRenderData.GVRRenderingOrder;
 import org.gearvrf.utility.Log;
 
 public class PlanesManager extends GVRScript implements PositionListener {
@@ -25,12 +29,20 @@ public class PlanesManager extends GVRScript implements PositionListener {
 		mGVRContext = _GVRContext;
 		mScore = new Score(this);
 		planeArray = new ArrayList<Planes>();
-
-		for(int i=0; i<count; i++){
+		for (int i = 0; i < count; i++) {
 			Planes myPlane = new Planes(mGVRContext, i);
 			myPlane.setPositionListener(this);
 			planeArray.add(myPlane);
 		}
+
+		GVRTexture texture = _GVRContext.loadTexture(new GVRAndroidResource(mGVRContext, R.drawable.pointer));
+		GVRSceneObject pointer = new GVRSceneObject(_GVRContext, 4.0f, 2.0f, texture);
+		pointer.getTransform().setPosition(0, 0, -1f);
+		pointer.getTransform().setScale(0.2f, 0.2f, 0.2f);
+		pointer.getRenderData().setDepthTest(false);
+		pointer.getRenderData().setRenderingOrder(GVRRenderingOrder.OVERLAY);
+		_GVRContext.getMainScene().getMainCameraRig().addChildObject(pointer);
+
 	}
 
 	public int getCount() {
@@ -43,10 +55,9 @@ public class PlanesManager extends GVRScript implements PositionListener {
 
 	}
 
-
 	@Override
 	public void onPositionChange(int index, double[] _position) {
-		//Log.i(TAG, "onPositionChange");
+		// Log.i(TAG, "onPositionChange");
 		mScore.updatePositionChange(index, _position);
 	}
 
@@ -55,7 +66,7 @@ public class PlanesManager extends GVRScript implements PositionListener {
 		mScore.updatePlaneShot(index);
 		Planes hitPlane = planeArray.get(index);
 		try {
-			//hitPlane.finalize();
+			// hitPlane.finalize();
 			Planes myPlane = new Planes(mGVRContext, index);
 			myPlane.setPositionListener(this);
 			planeArray.add(index, myPlane);
@@ -70,7 +81,7 @@ public class PlanesManager extends GVRScript implements PositionListener {
 		mScore.updatePlaneHit(index);
 		Planes hitPlane = planeArray.get(index);
 		try {
-			//hitPlane.finalize();
+			// hitPlane.finalize();
 			Planes myPlane = new Planes(mGVRContext, index);
 			myPlane.setPositionListener(this);
 			planeArray.add(index, myPlane);
